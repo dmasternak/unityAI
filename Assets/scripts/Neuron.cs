270818f3af1e5fd8c99661196e13554c5254422b
David Masternak <david.masternak@yahoo.com>
2015-05-19 18:38:18 +0200

    neuronalnetwork: Added neuron datastructure
    Neuron datastruct for neuronal network. fire method is already included

---
+++ Assets/scripts/Neuron.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Neuron {
+	double[] weight;
+	double   threshold;
+	List<Neuron> outputNeurons;
+
+	public Neuron(List<Neuron> outputNeurons) {
+		this.outputNeurons = outputNeurons;
+	}
+
+	public bool fire(double[] input) {
+		double sum = 0;
+		double length = Mathf.Min(weight.Length, outputNeurons.Count);
+
+		for(int i = 0; i < length; i++) {
+			sum += input[i] * weight[i];
+		}
+
+		if(sum >= threshold) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
